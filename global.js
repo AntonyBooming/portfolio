86c2523c5cda8259fdbec2da311b36d2b888de6a
Antony Munkhchuluun <165747900+AntonyBooming@users.noreply.github.com>
2026-04-21 15:33:57 -0700

    automating nav

--- global.js
+++ global.js
@@ -4,10 +4,33 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/";
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'Resume/', title: 'Resume' },
+  { url: 'https://github.com/AntonyBooming', title: 'GitHub' },
+];
 
-currentLink?.classList.add("current");
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+  a.toggleAttribute('target', a.host !== location.host);
+  a.target = a.host !== location.host ? '_blank' : '';
+}
