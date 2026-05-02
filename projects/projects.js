b15fec7e058fb13b1d25f3758719391b3dfa5b58
Antony Munkhchuluun <165747900+AntonyBooming@users.noreply.github.com>
2026-05-01 17:27:38 -0700

    update step 2.1, 2.2

--- projects/projects.js
+++ projects/projects.js
@@ -10,11 +10,18 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
-let data = [1, 2, 3, 4, 5];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-let sliceGenerator = d3.pie();
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -25,3 +32,11 @@ arcs.forEach((arc, idx) => {
         .attr('fill', colors(idx));
 });
 
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend
+        .append('li')
+        .attr('style', `--color:${colors(idx)}`)
+        .attr('class', 'legend-item')
+        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+});
